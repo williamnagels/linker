da0764cc2b0c47765ac4f64aa6941c1880617e08
william nagels <william.nagels@gmail.com>
2017-11-06 23:25:35 +0100

    Make memory map into shared ptr to be shared by input and output elfs

--- src/include/core/elf.h
+++ src/include/core/elf.h
@@ -6,33 +6,51 @@
 #include <boost/interprocess/file_mapping.hpp>
 #include <boost/interprocess/mapped_region.hpp>
 #include <string>
+#include <optional>
+#include <memory>
 namespace N_Core
 {
 	class Elf
 	{
 	public:
 		std::string _file_name;
-		boost::interprocess::mapped_region _region;
+
+		// Shared ptr because an output elf containing sections of this elf will need
+		// to extend the lifetime of the memory mapped region.
+		std::shared_ptr<boost::interprocess::mapped_region> _region; 
 		
 		N_Header::Header _header;
 		
-		
-		Elf(boost::interprocess::mapped_region&& mapped_region) :
-			_region(std::move(mapped_region)),
-			_header(N_Header::create_header_from_memory_blob(BinaryBlob(reinterpret_cast<uint8_t*>(_region.get_address()), reinterpret_cast<uint8_t*>(_region.get_address()) + _region.get_size())))
+		Elf(std::shared_ptr<boost::interprocess::mapped_region>&& mapped_region) :
+			_region(std::move(mapped_region))
+			, _header(N_Header::create_header_from_memory_blob(BinaryBlob(reinterpret_cast<uint8_t*>(_region->get_address()), reinterpret_cast<uint8_t*>(_region->get_address()) + _region->get_size())))
 		{
 		}
 
+		template<typename T>
+		Elf(T&& t):
+			Elf(std::forward<std::shared_ptr<boost::interprocess::mapped_region>>(t._region))
+		{
+			_file_name = std::forward<std::string>(t._file_name);
+		}
 
+		friend Elf create_elf_from_existing_elf(Elf, std::string const& path_to_elf);
 		friend Elf create_elf_from_path_to_file(std::string const& path_to_elf);
 	};
 
 	Elf create_elf_from_path_to_file(std::string const& path_to_elf)
 	{
 		boost::interprocess::file_mapping m_file(path_to_elf.c_str(), boost::interprocess::read_only);
-		boost::interprocess::mapped_region region(m_file, boost::interprocess::read_only);
-		return Elf(std::move(region)); //Mapped file will be closed when the Elf obj is destructed.
+		auto&& memory_region = std::make_shared<boost::interprocess::mapped_region>(m_file, boost::interprocess::read_only);
+
+		return Elf(std::forward<std::shared_ptr<boost::interprocess::mapped_region>>(memory_region)); //Mapped file will be closed when the Elf obj is destructed.
 	}
 
+	Elf create_elf_from_existing_elf(Elf existing_elf, std::string const& path_to_elf)
+	{
+		auto& elf = Elf(std::forward<Elf>(existing_elf));
+		elf._file_name = path_to_elf;
 
+		return elf;
+	}
 }
