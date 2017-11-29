14dcc8ae1b4b175b6b98569346572be0f2acddf6
william nagels <william.nagels@gmail.com>
2017-11-29 17:01:35 +0100

    Fix comment related to math branch.

--- src/include/core/general.h
+++ src/include/core/general.h
@@ -9,14 +9,18 @@ namespace N_Core
 	using BinaryBlob = boost::iterator_range<uint8_t*>; ///< A blob of bytes
 
 	/* @brief Mathematical branch
-	*	Avoid jumps and killing instruction pipelining. 
+	*	avoid jumps if not reqired. Problem is that pipeline may still get messed up because
+	*	the result depends on both input params.
+	*
 	*   Same as: auto value = (condition)?true_branch:false_branch;
 	*   Basically returns value without the jump statement in there.
 	*/
 	template <typename RV1, typename RV2>
 	inline auto math_branch(bool condition, RV1 true_branch, RV2  false_branch)
 	{
-		return true_branch & (condition  * std::numeric_limits<RV1>::max())
-			| false_branch & (not condition * std::numeric_limits<RV2>::max());
+		auto true_branch_val = true_branch & (condition  * std::numeric_limits<RV1>::max());
+		auto false_branch_val = false_branch & (not condition * std::numeric_limits<RV2>::max());
+		return true_branch_val | false_branch_val;
+			
 	};
 };
