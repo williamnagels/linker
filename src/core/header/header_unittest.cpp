1832ed4d9da1d912084c1b3e40a17424f7ef0494
william nagels <william.nagels@gmail.com>
2017-12-02 19:37:13 +0100

    reenable header unit tests

--- src/core/header/header_unittest.cpp
+++ src/core/header/header_unittest.cpp
@@ -8,54 +8,40 @@ BOOST_AUTO_TEST_SUITE(elf_header)
 // Not being able to parse elf header (magic byte = wrong)
 BOOST_AUTO_TEST_CASE(not_an_elf)
 {
-	/*
 	BOOST_CHECK_EXCEPTION(
 	N_Core::create_elf("testfiles/wrong_magic_bytes");, 
 	std::invalid_argument, 
-	HAS_MESSAGE(N_Core::N_Header::wrong_magic_bytes_message));*/
+	HAS_MESSAGE(N_Core::N_Header::wrong_magic_bytes_message));
 }
 
 BOOST_AUTO_TEST_CASE(parse_64bit_sleep)
 {
-	/*
-	boost::interprocess::file_mapping m_file("testfiles/sleep", boost::interprocess::read_only);
-	auto&& memory_region = std::make_shared<boost::interprocess::mapped_region>(m_file, boost::interprocess::read_only);
-	N_Core::Elf<N_Core::Bit64> elf(std::move(memory_region));
+	auto elf = N_Core::create_elf("testfiles/sleep");
 
-	auto header = elf._header.get();
-	
-	BOOST_CHECK_EQUAL(header.e_type, N_Core::N_Header::Type::ET_EXEC);
-	BOOST_CHECK_EQUAL(header.e_version, N_Core::N_Header::Version::EV_CURRENT);
-	BOOST_CHECK_EQUAL(header.e_ehsize, sizeof(N_Core::N_Header::Elf64_Ehdr));
-	BOOST_CHECK_EQUAL(header.e_phoff, sizeof(N_Core::N_Header::Elf64_Ehdr));
-	BOOST_CHECK_EQUAL(header.e_machine, N_Core::N_Header::Machine::EM_X86_64);
-	BOOST_CHECK_EQUAL(header.e_phnum, 9);
-	BOOST_CHECK_EQUAL(header.e_shnum, 27);
-	BOOST_CHECK_EQUAL(header.e_shstrndx, 26);
-	BOOST_CHECK_EQUAL(header.e_phentsize, 56);
-	BOOST_CHECK_EQUAL(header.e_shentsize, 64);
-	BOOST_CHECK_EQUAL(header.e_flags, 0);
-	BOOST_CHECK_EQUAL(header.e_shoff, 29480);
-	BOOST_CHECK_EQUAL(header.e_entry, 0x4017b9);
-	BOOST_CHECK_EQUAL(header.e_OS_ABI, N_Core::N_Header::OS_ABI::ELFOSABI_NONE);
-	BOOST_CHECK_EQUAL(header.e_ABI_version, 0);*/
+	BOOST_CHECK_EQUAL(elf._header->get_type(), N_Core::N_Header::Type::ET_EXEC);
+	BOOST_CHECK_EQUAL(elf._header->get_version(), N_Core::N_Header::Version::EV_CURRENT);
+	BOOST_CHECK_EQUAL(elf._header->get_elf_header_size(), sizeof(N_Core::N_Header::Elf64_Ehdr));
+	BOOST_CHECK_EQUAL(elf._header->get_program_header_offset(), sizeof(N_Core::N_Header::Elf64_Ehdr));
+	BOOST_CHECK_EQUAL(elf._header->get_machine(), N_Core::N_Header::Machine::EM_X86_64);
+	BOOST_CHECK_EQUAL(elf._header->get_program_header_number_of_entries(), 9);
+	BOOST_CHECK_EQUAL(elf._header->get_section_header_number_of_entries(), 27);
+	BOOST_CHECK_EQUAL(elf._header->get_section_index_that_contains_strings(), 26);
+	BOOST_CHECK_EQUAL(elf._header->get_program_header_entry_size(), 56);
+	BOOST_CHECK_EQUAL(elf._header->get_section_header_entry_size(), 64);
+	BOOST_CHECK_EQUAL(elf._header->get_flags(), 0);
+	BOOST_CHECK_EQUAL(elf._header->get_section_header_offset(), 29480);
+	BOOST_CHECK_EQUAL(elf._header->get_entry(), 0x4017b9);
+	BOOST_CHECK_EQUAL(elf._header->get_OS_ABI(), N_Core::N_Header::OS_ABI::ELFOSABI_NONE);
+	BOOST_CHECK_EQUAL(elf._header->get_ABI_version(), 0);
 }
 
 
 BOOST_AUTO_TEST_CASE(COW_header)
 {
-	/*
-	boost::interprocess::file_mapping m_file("testfiles/sleep", boost::interprocess::read_only);
-	auto&& memory_region = std::make_shared<boost::interprocess::mapped_region>(m_file, boost::interprocess::read_only);
-	N_Core::Elf<N_Core::Bit64> elf(std::move(memory_region));
-
-
-	auto header = elf._header.get();
-	BOOST_CHECK_EQUAL(header.e_type, N_Core::N_Header::Type::ET_EXEC);
-	header.e_type = N_Core::N_Header::Type::ET_DYN;
-	elf._header.set(header);
-	BOOST_CHECK_EQUAL(elf._header.get().e_type, N_Core::N_Header::Type::ET_DYN);
-	*/
+	auto elf = N_Core::create_elf("testfiles/sleep");
+	BOOST_CHECK_EQUAL(elf._header->get_type(), N_Core::N_Header::Type::ET_EXEC);
+	elf._header->set_type(N_Core::N_Header::Type::ET_DYN);
+	BOOST_CHECK_EQUAL(elf._header->get_type(), N_Core::N_Header::Type::ET_DYN);
 }
 
 
