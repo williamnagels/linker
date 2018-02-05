f088c39c032c1d65bb6be9ee99e0eaae9aedd1e4
william nagels <william.nagels@gmail.com>
2018-02-05 20:30:29 +0100

    symbol table iterator idea. Will implememt this on section aswell.

--- src/include/core/symtab/symbol_table.h
+++ src/include/core/symtab/symbol_table.h
@@ -8,6 +8,48 @@ namespace N_Core
 	namespace N_Symbol
 	{
 		template <typename T>
+		class Table;
+
+		template <typename T, bool IsConst>
+		class Iterator
+		{
+			uint16_t _current_index;
+			Table<T>& _table;
+
+		public:
+			Iterator(uint16_t index) :_current_index(index) {}
+			Iterator(Iterator const& o) :
+				_current_index(o._current_index),
+				_table(o._table) {};
+
+			Iterator<T, IsConst>& operator++()
+			{
+				_current_index++;
+				return *this;
+			}
+			Iterator<T, IsConst> operator++(int)
+			{
+				auto current_iterator = *this;
+				++*this;
+				return current_iterator;
+			}
+
+			using ValueType =
+				std::conditional_t<
+				IsConst
+				, std::add_const_t<Symbol<T>>
+				, Symbol<T>>;
+			ValueType operator*()
+			{
+				auto begin_it = _table.get_content().begin();
+				std::advance(begin_it, index);
+				auto end_it = begin_it;
+				std::advance(end_it, sizeof(T));
+				return ValueType(begin_it, end_it);
+			}
+		};
+
+		template <typename T>
 		class Table
 		{
 			MMap::Container<uint8_t> _content;
@@ -20,8 +62,14 @@ namespace N_Core
 			Table(BinaryBlob blob):
 				_content(blob.begin(), blob.end()){}
 
-		private:
-			std::vector<SymbolTy> _entries; ///< index in the vector is the 'symbol index' as used in elf.
+			Iterator<T, true> begin() const
+			{
+				return Iterator<T, true>(0, *this);
+			}
+			Iterator<T, true> end() const 
+			{
+				return Iterator<T, true>(get_content().get_size(), *this);
+			}
 		};
 
 	}
